eae48efd129629f48a8a05fd2caa511e99e1d67e
0w0mewo <72908579+0w0mewo@users.noreply.github.com>
2025-05-22 13:17:08 +0000

    make sure offical mobile app can discover the CLI device

--- internal/localsend/constants/paths.go
+++ internal/localsend/constants/paths.go
@@ -4,7 +4,7 @@ const (
 	UploadPath      = "/api/localsend/v2/upload"
 	PreuploadPath   = "/api/localsend/v2/prepare-upload"
 	CancelPath      = "/api/localsend/v2/cancel"
-	InfoPath        = "/api/localsend/v2/info"
+	InfoPath        = "/api/localsend/v*/info"
 	DownloadPath    = "/api/localsend/v2/download"
 	PreDownloadPath = "/api/localsend/v2/prepare-download"
 )
